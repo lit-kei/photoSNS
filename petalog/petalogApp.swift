25c39a9f636ac415b1620470915c8e2fbdd3f691
lit-kei <puyoteto123@yahoo.co.jp>
2026-08-14 15:43:34 +0900

    Initial commit

---
+++ petalog/petalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  petalogApp.swift
+//  petalog
+//
+//  Created by 敬祐 on 2026/08/14.
+//
+
+import SwiftUI
+
+@main
+struct petalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
